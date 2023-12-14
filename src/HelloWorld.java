2b05f063c9ad30129e7751ed050d4c5c9a357639
er-waseem-ansari <erwaseem26@gmail.com>
2023-12-14 11:11:10 +0530

    Random commit of hexa

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+        public static void main(String[] args) {
+                String str1 = "Employee";
+                String str2 = "Company";
+                if (str1.equals(str2)) {
+                        System.out.println("Strings are equal");
+                } else {
+                        System.out.println("Strings are not equal");
+                }
+                System.out.println("Length of str1: " + str1.length());
+                System.out.println("Length of str2: " + str2.length());
+                System.out.println("Substring of str1 from index 4: " + str1.substring(4));
+                String concatenatedString = str1.concat(str2);
+                System.out.println("Concatenated string: " + concatenatedString);
+                System.out.println("str1 in lowercase: " + str1.toLowerCase());
+                System.out.println("str2 in uppercase: " + str2.toUpperCase());
+                int indexOfSubstring = str1.indexOf("lo");
+                System.out.println("Index of 'lo' in str1: " + indexOfSubstring);
+        }
+}
---
+++ src/Main.java
@@ -0,0 +1,73 @@
+//Scanner sc = new Scanner(System.in);
+
+//System.out.println(Arrays.toString(array));
+
+//String st = sc.nextLine();
+//String [] array = st.split(";");
+
+//int [] cloneArray = intArray.clone();
+
+//ArrayList<String> cars = new ArrayList<String>();
+//cars.add("waseem");
+//cars.add("ansari");
+//cars.add("ansari");
+//System.out.println(cars.size());
+//Collections.sort(cars);
+
+//StringFunction exclaim = (s) -> s + "!";
+//
+
+
+//HashMap<String, Integer> map = new HashMap<>();
+//
+//        map.put("vishal", 10);
+//        map.put("sachin", 30);
+//        map.put("vaibhav", 20);
+//
+//        System.out.println("Size of map is:- "
+//        + map.size());
+//
+//        System.out.println(map);
+//
+//        if (map.containsKey("vishal")) {
+//
+//        Integer a = map.get("vishal");
+//
+//        System.out.println("value for key"
+//        + " \"vishal\" is:- " + a);
+//        }
+
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
